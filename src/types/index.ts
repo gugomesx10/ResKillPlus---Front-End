6a40b0f965f50c1545a5cd7616e2f98ab6ed8ea5
gugomesx@10 <gustavogomesmartins4@icloud.com>
2025-11-18 21:33:16 -0300

    feat: adiciona tipos e interfaces do projeto

---
+++ src/types/index.ts
@@ -0,0 +1,56 @@
+export interface Usuario {
+  cpf: string;
+  nome: string;
+  email: string;
+  senha?: string;
+  telefone?: string;
+  dataNascimento?: string;
+}
+
+export interface Curso {
+  nome: string;
+  descricao: string;
+  cargaHoraria: number;
+  categoria: string;
+  nivel?: string;
+}
+
+export interface Habilidade {
+  nome: string;
+  descricao: string;
+  categoria: string;
+  nivelDificuldade?: string;
+}
+
+export interface Matricula {
+  cpfUsuario: string;
+  nomeCurso: string;
+  dataMatricula: string;
+  status: string;
+  progresso?: number;
+}
+
+export interface Recomendacao {
+  id?: number;
+  cpf: string;
+  nomeCurso: string;
+  motivo: string;
+  dataRecomendacao?: string;
+}
+
+export interface Pagamento {
+  id?: number;
+  cpfUsuario: string;
+  nomeCurso: string;
+  valor: number;
+  metodoPagamento: string;
+  status: string;
+  dataPagamento?: string;
+}
+
+export interface AuthUser {
+  nome: string;
+  email: string;
+  avatar?: string;
+  accessToken: string;
+}
