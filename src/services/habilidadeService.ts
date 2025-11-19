e46ac09baeceab2f364dbafef3e825c98291f583
gugomesx@10 <gustavogomesmartins4@icloud.com>
2025-11-18 21:43:57 -0300

    feat: adiciona serviço de habilidades

---
+++ src/services/habilidadeService.ts
@@ -0,0 +1,24 @@
+import api from './api';
+import { Habilidade } from '../types';
+
+export const habilidadeService = {
+  criar: (habilidade: Habilidade) => {
+    return api.post('/habilidade', habilidade);
+  },
+
+  buscarPorNome: (nome: string) => {
+    return api.get(`/habilidade/buscar/${nome}`);
+  },
+
+  listarTodas: () => {
+    return api.get('/habilidade');
+  },
+
+  atualizar: (habilidade: Habilidade) => {
+    return api.put('/habilidade', habilidade);
+  },
+
+  excluir: (nome: string) => {
+    return api.delete(`/habilidade/excluir/${nome}`);
+  },
+};
