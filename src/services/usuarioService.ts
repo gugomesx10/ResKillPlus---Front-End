7a140e97c8c6cb7ac6df27f89f0da86d2b787b0c
gugomesx@10 <gustavogomesmartins4@icloud.com>
2025-11-18 21:42:53 -0300

    feat: adiciona serviço de usuários

---
+++ src/services/usuarioService.ts
@@ -0,0 +1,24 @@
+import api from './api';
+import { Usuario } from '../types';
+
+export const usuarioService = {
+  criar: (usuario: Usuario) => {
+    return api.post('/usuario', usuario);
+  },
+
+  buscarPorCpf: (cpf: string) => {
+    return api.get(`/usuario/buscar/${cpf}`);
+  },
+
+  validar: (cpf: string, senha: string) => {
+    return api.get(`/usuario/validar/${cpf}/${senha}`);
+  },
+
+  atualizar: (usuario: Usuario) => {
+    return api.put('/usuario', usuario);
+  },
+
+  excluir: (cpf: string) => {
+    return api.delete(`/usuario/excluir/${cpf}`);
+  },
+};
