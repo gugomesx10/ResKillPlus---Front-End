26526ff148394ff7bd937698388d36edaa5b105b
gugomesx@10 <gustavogomesmartins4@icloud.com>
2025-11-18 23:38:23 -0300

    feat: adiciona validação de email

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -14,29 +14,38 @@ import Recomendacoes from './routes/Recomendacoes';
 import Pagamentos from './routes/Pagamentos';
 import Login from './routes/Login';
 
+function AppContent() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-950">
+      {!isLoginPage && <Header />}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/integrantes" element={<Integrantes />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/cursos" element={<Cursos />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/habilidades" element={<Habilidades />} />
+          <Route path="/matriculas" element={<Matriculas />} />
+          <Route path="/recomendacoes" element={<Recomendacoes />} />
+          <Route path="/pagamentos" element={<Pagamentos />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      {!isLoginPage && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="flex flex-col min-h-screen bg-white dark:bg-gray-950">
-          <Header />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<Sobre />} />
-              <Route path="/integrantes" element={<Integrantes />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/cursos" element={<Cursos />} />
-              <Route path="/usuarios" element={<Usuarios />} />
-              <Route path="/habilidades" element={<Habilidades />} />
-              <Route path="/matriculas" element={<Matriculas />} />
-              <Route path="/recomendacoes" element={<Recomendacoes />} />
-              <Route path="/pagamentos" element={<Pagamentos />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
